c477a343d514b7f7452b3bc58d09f8bf00abe9ac
Lawrence Yoon <yoonlawrence@yahoo.com>
2021-04-13 11:41:35 -0700

    testing - then or catch always moves to next then func confirmed

--- src/main.js
+++ src/main.js
@@ -10,3 +10,18 @@ tellPromise.catch(console.error).then((fortune) => {
   console.log(question);
   console.log(fortune);
 });
+
+// welcome()
+//   .then(console.log) // Logs the result of welcome()
+//   .then(goodbye) // Returns promise from goodbye()
+//   .then(console.log) // Logs the result of goodbye()
+//   .catch(console.error);
+
+welcome()
+  .then(console.log) // Logs the result of welcome()
+  .then(tell) // Calls tell, which returns a rejected promise (no question supplied).
+  .then(console.log) // Skipped because tell returned a rejected promise.
+  .catch(console.error) // Logs error from tell() or welcome()
+  .then(goodbye) // Returns promise from goodbye()
+  .then(console.log) // Logs the result from goodbye()
+  .catch(console.error); // Logs error only from goodbye()
